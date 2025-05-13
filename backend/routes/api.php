131771eab1ee886a71953e23d19779ee5b7fffaa
Kleber Souza <klsdesigner@hotmail.com>
2025-05-13 17:35:50 -0300

    Ajuste no docker-compose

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+Route::get('/', function (Request $request) {
+    return response()->json([
+        'status' => 'success',
+        'version' => 'v1',
+        'baseUrl' => 'http://localhost:9000/api',
+    ]);
+})->name('api.welcome'); 
--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Esta é minha pagina abaute</h1>
   </div>
 </template>
 
