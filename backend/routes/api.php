a36755b062977a1d17cf44fdb8deb8bca6cf7e6c
Kleber Souza <klsdesigner@hotmail.com>
2025-05-15 16:53:36 -0300

    Rota status api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,11 +6,20 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/user', function (Request $request) {
 //     return $request->user();
 // })->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::get('/', function (Request $request) {
+        return response()->json([
+            'status' => 'success',
+            'version' => 'v1',
+            'baseUrl' => 'http://localhost:9000/api/v1',
+        ], 200);
+    }); 
 
-Route::get('/', function (Request $request) {
-    return response()->json([
-        'status' => 'success',
-        'version' => 'v1',
-        'baseUrl' => 'http://localhost:9000/api',
-    ]);
-})->name('api.welcome'); 
+
+    // Route::get('/users', 'UserController@index')->name('api.users.index');
+    // Route::get('/users/{user}', 'UserController@show')->name('api.users.show');
+    // Route::post('/users', 'UserController@store')->name('api.users.store');
+    // Route::put('/users/{user}', 'UserController@update')->name('api.users.update');
+    // Route::delete('/users/{user}', 'UserController@destroy')->name('api.users.destroy');
+});
+
